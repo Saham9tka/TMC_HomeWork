8d58f05d7b6ead20f4368e6b73a57edf8bc8e93f
Saham9tka <sam9142026@gmail.com>
2024-08-29 14:58:06 +0300

    TMC_31

---
+++ src/Animal/MyMain.java
@@ -0,0 +1,24 @@
+package Animal;
+
+public class MyMain {
+        public static void main(String[] args) {
+            // Создание объектов для каждого животного
+            Animal dog = new Dog();
+            Animal tiger = new Tiger();
+            Animal rabbit = new Rabbit();
+
+            // Тестирование методов
+            dog.voice(); // Собака издает звук
+            dog.eat("Meat"); // Собака ест мясо
+            dog.eat("Grass"); // Собака не любит траву
+
+            tiger.voice(); // Тигр издает звук
+            tiger.eat("Meat"); // Тигр ест мясо
+            tiger.eat("Grass"); // Тигр не любит траву
+
+            rabbit.voice(); // Кролик издает звук
+            rabbit.eat("Grass"); // Кролик ест траву
+            rabbit.eat("Meat"); // Кролик не любит мясо
+        }
+
+}
