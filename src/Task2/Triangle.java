0ca83046c0f8f6e0ab6d92d9e0208092e96036d8
Saham9tka <sam9142026@gmail.com>
2024-09-03 16:33:49 +0300

    TMC_31

--- src/Task2/Triangle.java
+++ src/Task2/Triangle.java
@@ -4,25 +4,21 @@ public class Triangle extends Figure {
     private final double A;
     private final double B;
     private final double C;
-
     public Triangle(double A, double B, double C) {
         this.A = A;
         this.B = B;
         this.C = C;
     }
-
     @Override
     public double getPerimeter() {
         return this.A + this.B + this.C;
     }
-
     @Override
     public double getSquare() {
         double HalfPerimeter = getPerimeter() / 2;
         return Math.sqrt(HalfPerimeter * (HalfPerimeter - this.A) *
                 (HalfPerimeter - this.B) * (HalfPerimeter - this.C));
     }
-
     @Override
     public String toString() {
         return "The triangle has sides A = " + A + ", B = " + B + ", C = " + C +
