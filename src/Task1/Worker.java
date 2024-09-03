92904757f16715bd62781031c1e32fcbea6ce4c8
Saham9tka <sam9142026@gmail.com>
2024-09-03 15:20:54 +0300

    TMC_31

--- src/Task1/Worker.java
+++ src/Task1/Worker.java
@@ -1,13 +1,9 @@
 package Task1;
 
 public class Worker extends Person{
-
     public Worker(String name) {
-        super(name, "Worker");
-    }
-
+        super(name, "Worker");}
     @Override
     public void doResponsibilities() {
-        System.out.println("The worker is in charge of different activities.");
-    }
+        System.out.println("The worker is in charge of different activities.");}
 }
