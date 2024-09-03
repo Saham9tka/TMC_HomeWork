8dedbe46685344c5a05efee1369350a3ed98e6ae
Saham9tka <sam9142026@gmail.com>
2024-09-03 15:24:14 +0300

    TMC_31

--- src/Task2/Circle.java
+++ src/Task2/Circle.java
@@ -20,8 +20,8 @@ public class Circle extends Figure {
     @Override
     public String toString() {
         return "Your figure is Circle" +
-                "\nIts radius is: " + this.radius +
-                "\nIts perimeter is: " + getPerimeter() +
-                "\nIts square is: " + getSquare();
+                "\n radius is: " + this.radius +
+                "\n perimeter is: " + getPerimeter() +
+                "\n square is: " + getSquare();
     }
 }
