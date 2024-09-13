f6e113f5449547c2c917a2200b96f933daa52d62
Saham9tka <sam9142026@gmail.com>
2024-09-13 15:12:31 +0300

    TMC_31

---
+++ src/RegexExample.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexExample {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите строку:");
+        String input = scanner.nextLine();
+        System.out.println("Аббревиатуры:");
+        findAbbreviations(input);
+    }
+    private static void findAbbreviations(String input) {
+        String pattern = "[A-Z]{2,6}";
+        Pattern regex = Pattern.compile(pattern);
+        Matcher matcher = regex.matcher(input);
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
+    }
+}
